e91383c96f79e5d6782e2d32b0d3dac401c4a689
bopaki <btebalo@gmail.com>
2023-06-20 23:49:23 +0200

    Updated readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,11 @@ import {Alert, Dropdown, Navbar} from "flowbite-react"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono lg:flex">
-        <article className='max-w-none format lg:format-lg format-red dark:format-invert'>
-          <h1>Prototyping from A to Z: best practices for successful prototypes</h1>
-          <p className='lead'>Flowbite is an open-source library of UI components built with the utility-first classes from Tailwind CSS. It also includes interactive elements such as dropdowns, modals, datepickers.</p>
-          <p>Before going digital, you might benefit from scribbling down some ideas in a sketchbook. This way, you can think things things through before committing to an actual design project.</p>
-          <p>But then I found a <a href="#">component library based on Tailwind CSS called Flowbite</a>. It comes with the most commonly used UI components, such as buttons, navigation bars, cards, form elements, and more which are conveniently built with the utility classes from Tailwind CSS.</p>
-          <div className=''>
-          <h2>When does design come in handy?</h2>
-          <p>While it might seem like extra work at a first glance, here are some key moments in which prototyping will come in handy:</p>
-              <ol>
-                <li><strong>Usability testing</strong>. Does your user know how to exit out of screens? Can they follow your intended user journey and buy something from the site you’ve designed? By running a usability test, you’ll be able to see how users will interact with your design once it’s live;</li>
-                <li><strong>Involving stakeholders</strong>. Need to check if your GDPR consent boxes are displaying properly? Pass your prototype to your data protection team and they can test it for real;</li>
-                <li><strong>Impressing a client</strong>. Prototypes can help explain or even sell your idea by providing your client with a hands-on experience;</li>
-                <li><strong>Communicating your vision</strong>. By using an interactive medium to preview and test design elements, designers and developers can understand each other — and the project — better.</li>
-              </ol>
-          </div>
-        </article>
-      </div>
+<div className="grid grid-rows-3 grid-flow-col gap-4">
+  <div className="row-span-3">01</div>
+  <div className="col-span-2">02</div>
+  <div className="row-span-2 col-span-2 ...">03</div>
+</div>
     </main>
   )
 }
