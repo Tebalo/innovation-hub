17f626ab4e71e756a77d9e21e2b68c5663bdea68
bopaki <btebalo@gmail.com>
2023-06-22 00:42:11 +0200

    UI wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,60 +4,63 @@ import {Alert, Dropdown, Navbar} from "flowbite-react"
 export default function Home() {
   return (
     <>
-    <header className="bg-white py-4">
-    <div className="container mx-auto px-4">
-      <nav className="flex justify-between items-center">
-        <a href="#" className="text-lg font-semibold text-gray-800">Logo</a>
-        <ul className="flex space-x-4">
-          <li><a href="#" className="text-gray-600 hover:text-gray-800">Home</a></li>
-          <li><a href="#" className="text-gray-600 hover:text-gray-800">About</a></li>
-          <li><a href="#" className="text-gray-600 hover:text-gray-800">Services</a></li>
-          <li><a href="#" className="text-gray-600 hover:text-gray-800">Contact</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-
   <main className="container mx-auto px-4 py-8">
-    <h1 className="text-4xl font-semibold text-gray-800">Welcome to Our Website</h1>
-    <p className="mt-4 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in porta metus, sed
-      aliquam nulla. Mauris ac tristique est, at efficitur massa. Nulla facilisi. Aliquam erat volutpat. Sed luctus
-      faucibus est, vel feugiat diam hendrerit et.</p>
-    <button className="mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600">Get Started</button>
-    <div className="mt-8 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-  <div className="bg-white rounded-lg shadow-lg p-6">
-    <h2 className="text-lg font-semibold text-gray-800">Service 1</h2>
-    <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut erat
-      sagittis, tincidunt nisi et, interdum libero. Nulla facilisi.</p>
-  </div>
-  <div className="bg-white rounded-lg shadow-lg p-6">
-    <h2 className="text-lg font-semibold text-gray-800">Service 2</h2>
-    <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut erat
-      sagittis, tincidunt nisi et, interdum libero. Nulla facilisi.</p>
-  </div>
-  <div className="bg-white rounded-lg shadow-lg p-6">
-    <h2 className="text-lg font-semibold text-gray-800">Service 3</h2>
-    <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut erat
-      sagittis, tincidunt nisi et, interdum libero. Nulla facilisi.</p>
-  </div>
-  <div className="bg-white rounded-lg shadow-lg p-6">
-    <h2 className="text-lg font-semibold text-gray-800">Service 3</h2>
-    <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut erat
-      sagittis, tincidunt nisi et, interdum libero. Nulla facilisi.</p>
-  </div>
-  <div className="bg-white rounded-lg shadow-lg p-6">
-    <h2 className="text-lg font-semibold text-gray-800">Service 3</h2>
-    <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut erat
-      sagittis, tincidunt nisi et, interdum libero. Nulla facilisi.</p>
-  </div>
-  <div className="bg-white rounded-lg shadow-lg p-6">
-    <h2 className="text-lg font-semibold text-gray-800">Service 3</h2>
-    <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut erat
-      sagittis, tincidunt nisi et, interdum libero. Nulla facilisi.</p>
-  </div>
-  
-</div>
 
+    <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Better Data</span> Scalable AI.</h1>
+    <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+    <p className="mb-3 text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+    <p className="text-gray-500 dark:text-gray-400">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+
+    <button className="mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600">Get Started</button>
+    <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+<figure className="max-w-screen-md">
+    <div className="flex items-center mb-4 text-yellow-300">
+        <svg aria-hidden="true" className="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        <svg aria-hidden="true" className="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        <svg aria-hidden="true" className="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        <svg aria-hidden="true" className="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+        <svg aria-hidden="true" className="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+    </div>
+    <blockquote>
+        <p className="text-2xl font-semibold text-gray-900 dark:text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+    </blockquote>
+    <figcaption className="flex items-center mt-6 space-x-3">
+        <img className="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="profile picture"></img>
+        <div className="flex items-center divide-x-2 divide-gray-300 dark:divide-gray-700">
+            <cite className="pr-3 font-medium text-gray-900 dark:text-white">Bonnie Green</cite>
+            <cite className="pl-3 text-sm text-gray-500 dark:text-gray-400">CTO at Flowbite</cite>
+        </div>
+    </figcaption>
+</figure>
+<hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+<figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+  <a href="#">
+    <img className="rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" alt="image description"></img>
+  </a>
+  <figcaption className="absolute px-4 text-lg text-white bottom-6">
+      <p>Do you want to get notified when a new component is added to Flowbite?</p>
+  </figcaption>
+</figure>
+
+<hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+
+<div className="mt-8 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </a>
+    </div>
+</div>
+</div>
   </main>
 
   <footer className="bg-gray-800 py-4">
