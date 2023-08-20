9a0c3886a170ae3e4697dc7dd647e753ed64d7a3
bopaki <btebalo@gmail.com>
2023-08-20 16:04:32 +0200

    Installed shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Taxonomy',
+  title: 'Innovation Hub',
   description: 'Prototyping..',
 }
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+      </body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,76 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    './node_modules/flowbite-react/**/*.js',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      screens:{
-        'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
-        'laptop': '1024px',
-        // => @media (min-width: 1024px) { ... }
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
-      typography: ({ theme }) => ({
-        orange: {
-          css: {
-            '--tw-prose-body': theme('colors.orange[500]'),
-            '--tw-prose-headings': theme('colors.orange[900]'),
-            '--tw-prose-lead': theme('colors.orange[500]'),
-            '--tw-prose-links': theme('colors.orange[600]'),
-            '--tw-prose-bold': theme('colors.orange[900]'),
-            '--tw-prose-counters': theme('colors.orange[500]'),
-            '--tw-prose-bullets': theme('colors.orange[500]'),
-            '--tw-prose-hr': theme('colors.orange[200]'),
-            '--tw-prose-quotes': theme('colors.orange[900]'),
-            '--tw-prose-quote-borders': theme('colors.orange[300]'),
-            '--tw-prose-captions': theme('colors.orange[700]'),
-            '--tw-prose-code': theme('colors.orange[900]'),
-            '--tw-prose-code-bg': theme('colors.orange[50]'),
-            '--tw-prose-pre-code': theme('colors.orange[100]'),
-            '--tw-prose-pre-bg': theme('colors.orange[900]'),
-            '--tw-prose-th-borders': theme('colors.orange[300]'),
-            '--tw-prose-td-borders': theme('colors.orange[200]'),
-            '--tw-format-th-bg': theme('colors.orange[50]'),
-            '--tw-prose-invert-body': theme('colors.orange[200]'),
-            '--tw-prose-invert-headings': theme('colors.white'),
-            '--tw-prose-invert-lead': theme('colors.orange[300]'),
-            '--tw-prose-invert-links': theme('colors.white'),
-            '--tw-prose-invert-bold': theme('colors.white'),
-            '--tw-prose-invert-counters': theme('colors.orange[400]'),
-            '--tw-prose-invert-bullets': theme('colors.orange[600]'),
-            '--tw-prose-invert-hr': theme('colors.orange[700]'),
-            '--tw-prose-invert-quotes': theme('colors.pink[100]'),
-            '--tw-prose-invert-quote-borders': theme('colors.orange[700]'),
-            '--tw-prose-invert-captions': theme('colors.orange[400]'),
-            '--tw-prose-invert-code': theme('colors.white'),
-            '--tw-prose-invert-pre-code': theme('colors.orange[300]'),
-            '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
-            '--tw-prose-invert-th-borders': theme('colors.orange[600]'),
-            '--tw-prose-invert-td-borders': theme('colors.orange[700]'),
-            '--tw-format-invert-th-bg': theme('colors.orange[700]'),
-          },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-      }),
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [
-    require('flowbite-typography'),
-    require("flowbite/plugin")
-  ],
+  plugins: [require("tailwindcss-animate")],
 }
